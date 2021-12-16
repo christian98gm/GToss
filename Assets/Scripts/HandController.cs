6e05ec25950b2650540dd71820a4d2762d729c59
unknown <christian98gm>
2021-12-16 18:19:00 +0100

    Hand animation and controls work in Oculus. PC pending...

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -6,16 +6,78 @@ using UnityEngine.XR;
 public class HandController : MonoBehaviour
 {
 
+    private const float TRIGGER_THRESHOLD = 0.1f;
+
     public GameObject controllerPrefab;
+    public InputDeviceCharacteristics controllerCharacteristics;
+
     private InputDevice targetDevice;
+    private GameObject spawnedHand;
+    private Animator handAnimator;
+
+    private bool pcMode = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        TryInitialize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(pcMode) {
+            UpdatePCHandAnimation();
+        } else {
+            if(!targetDevice.isValid) {
+                TryInitialize();
+            } else {
+                UpdateDeviceHandAnimation();
+            }
+        }
+    }
+
+    void TryInitialize()
+    {
+
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+        if(devices.Count > 0) {
+            targetDevice = devices[0];
+            Debug.Log(targetDevice.name);
+        }
+
+        spawnedHand = Instantiate(controllerPrefab, transform);
+        handAnimator = spawnedHand.GetComponent<Animator>();
+
+    }
+
+    void UpdateDeviceHandAnimation()
+    {
+        //Trigger
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue)) {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+        //Grip
+        if(targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue)) {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void UpdatePCHandAnimation()
+    {
+        Debug.Log("PC Mode ON");
+    }
+
+    public void SetPCMode(bool mode) {
+        pcMode = mode;
     }
 }
--- Assets/Scripts/XRPCController.cs
+++ Assets/Scripts/XRPCController.cs
@@ -8,7 +8,8 @@ using UnityEngine.XR;
 public class XRPCController : MonoBehaviour
 {
 
-    public TrackedPoseDriver target;
+    public TrackedPoseDriver targetTracker;
+    public List<GameObject> controllers;
 
     public InputActionProperty pcPositionInput;
     public InputActionProperty pcRotationInput;
@@ -16,18 +17,47 @@ public class XRPCController : MonoBehaviour
     public InputActionProperty devicePositionInput;
     public InputActionProperty deviceRotationInput;
 
+    private bool isPC = false;
+    private bool firstTime = true;
+
     // Start is called before the first frame update
-    void Start() {
-        if(!XRSettings.isDeviceActive) {
+    void Start()
+    {
+        if(!XRSettings.isDeviceActive)
+        {
             Debug.Log("No headset is plugged");
-        } else if(XRSettings.isDeviceActive && XRSettings.loadedDeviceName == "MockHMD Display") {
-            target.positionInput = pcPositionInput;
-            target.rotationInput = pcRotationInput;
+        }
+        else if(XRSettings.isDeviceActive && XRSettings.loadedDeviceName == "MockHMD Display")
+        {
+            targetTracker.positionInput = pcPositionInput;
+            targetTracker.rotationInput = pcRotationInput;
+            isPC = true;
             Debug.Log("PC keyboard and mouse are plugged");
-        } else {
-            target.positionInput = devicePositionInput;
-            target.rotationInput = deviceRotationInput;
+        }
+        else
+        {
+            targetTracker.positionInput = devicePositionInput;
+            targetTracker.rotationInput = deviceRotationInput;
+            transform.GetChild(0).gameObject.SetActive(false);
             Debug.Log("Headset is plugged");
+        }
+    }
+
+    void Update()
+    {
+        if(firstTime)
+        {
+            firstTime = false;
+            if(isPC)
+            {
+                foreach(var item in controllers)
+                {
+                    HandController hc = item.GetComponentInChildren<HandController>();
+                    if(hc != null) {
+                        hc.SetPCMode(true);
+                    }
+                }
+            }
         }
     }
 
