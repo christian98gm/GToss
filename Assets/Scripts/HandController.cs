fbb5f00753f19a7997416cb6bd29ca92ae339397
unknown <christian98gm@gmail.com>
2021-12-17 20:33:05 +0100

    HandGrab branch finished. Player ready at XROriginPlayer Prefab

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -2,100 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
-using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandController : MonoBehaviour
 {
 
-    private const float TRIGGER_THRESHOLD = 0.1f;
-
     public GameObject controllerPrefab;
-    public InputDeviceCharacteristics controllerCharacteristics;
+    private const float speed = 10.0f;
 
-    private UnityEngine.XR.InputDevice targetDevice;
     private ActionBasedController targetController;
     private GameObject spawnedHand;
     private Animator handAnimator;
 
-    private XRDeviceSimulator deviceSimulator = null;
-    private InputActionReference deviceController;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent = 0.0f;
+    private float triggerCurrent = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         targetController = GetComponentInParent<ActionBasedController>();
-        TryInitialize();
+        spawnedHand = Instantiate(controllerPrefab, transform);
+        handAnimator = spawnedHand.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(deviceSimulator != null) {
-            UpdatePCHandAnimation();
-        } else {
-            if(!targetDevice.isValid) {
-                TryInitialize();
-            } else {
-                UpdateDeviceHandAnimation();
-            }
-        }
+        UpdateHandAnimation();
     }
 
-    void TryInitialize()
+    void UpdateHandAnimation()
     {
-
-        List<UnityEngine.XR.InputDevice> devices = new List<UnityEngine.XR.InputDevice>();
-        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
-        if(devices.Count > 0) {
-            targetDevice = devices[0];
-            Debug.Log(targetDevice.name);
-        }
-
-        spawnedHand = Instantiate(controllerPrefab, transform);
-        handAnimator = spawnedHand.GetComponent<Animator>();
-
-    }
-
-    void UpdateDeviceHandAnimation()
-    {
+        //TODO: Smooth tranasitioning
         //Trigger
-        if(targetDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.trigger, out float triggerValue)) {
-            handAnimator.SetFloat("Trigger", triggerValue);
-        }
-        else
+        triggerTarget = targetController.activateAction.action.ReadValue<float>();
+        if(triggerCurrent != triggerTarget)
         {
-            handAnimator.SetFloat("Trigger", 0);
+           triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+           handAnimator.SetFloat("Trigger", triggerCurrent);
         }
+        //handAnimator.SetFloat("Trigger", targetController.activateAction.action.ReadValue<float>());
         //Grip
-        if(targetDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out float gripValue)) {
-            handAnimator.SetFloat("Grip", gripValue);
-        }
-        else
+        gripTarget = targetController.selectAction.action.ReadValue<float>();
+        if(gripCurrent != gripTarget)
         {
-            handAnimator.SetFloat("Grip", 0);
+           gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+           handAnimator.SetFloat("Grip", gripCurrent);
         }
+        //handAnimator.SetFloat("Grip", targetController.selectAction.action.ReadValue<float>());
     }
 
-    void UpdatePCHandAnimation()
+    void SetGrip(float v)
     {
-        /*//Trigger
-        if(deviceController.action.ReadValue<float>() == 1) {
-            handAnimator.SetFloat("Trigger", deviceSimulator.triggerAction.action.ReadValue<float>());
-        }
-        //Grip
-        if(deviceController.action.ReadValue<float>() == 1) {
-            handAnimator.SetFloat("Grip", deviceSimulator.gripAction.action.ReadValue<float>());
-        }*/
-        //Trigger
-        handAnimator.SetFloat("Trigger", targetController.activateAction.action.ReadValue<float>());
-        //Grip
-        handAnimator.SetFloat("Grip", targetController.selectAction.action.ReadValue<float>());
+        gripTarget = v;
     }
 
-    public void SetPCMode(XRDeviceSimulator simulator, InputActionReference controller) {
-        deviceSimulator = simulator;
-        deviceController = controller;
+    void SetTrigger(float v) {
+        triggerTarget = v;
     }
 }
--- Assets/Scripts/XRPCController.cs
+++ Assets/Scripts/XRPCController.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 public class XRPCController : MonoBehaviour
 {
 
     public TrackedPoseDriver targetTracker;
-    public List<GameObject> controllers;
-    private XRDeviceSimulator deviceSimulator;
 
     public InputActionProperty pcPositionInput;
     public InputActionProperty pcRotationInput;
@@ -19,13 +16,9 @@ public class XRPCController : MonoBehaviour
     public InputActionProperty devicePositionInput;
     public InputActionProperty deviceRotationInput;
 
-    private bool isPC = false;
-    private bool firstTime = true;
-
     // Start is called before the first frame update
     void Start()
     {
-        deviceSimulator = GetComponentInChildren<XRDeviceSimulator>();
         if(!XRSettings.isDeviceActive)
         {
             Debug.Log("No headset is plugged");
@@ -34,7 +27,6 @@ public class XRPCController : MonoBehaviour
         {
             targetTracker.positionInput = pcPositionInput;
             targetTracker.rotationInput = pcRotationInput;
-            isPC = true;
             Debug.Log("PC keyboard and mouse are plugged");
         }
         else
@@ -43,24 +35,6 @@ public class XRPCController : MonoBehaviour
             targetTracker.rotationInput = deviceRotationInput;
             transform.GetChild(0).gameObject.SetActive(false);
             Debug.Log("Headset is plugged");
-        }
-    }
-
-    void Update()
-    {
-        if(firstTime)
-        {
-            firstTime = false;
-            if(isPC)
-            {
-                for(int i = 0; i < controllers.Count; i++)
-                {
-                    HandController hc = controllers[i].GetComponentInChildren<HandController>();
-                    if(hc != null) {
-                        hc.SetPCMode(deviceSimulator, i == 0 ? deviceSimulator.manipulateLeftAction : deviceSimulator.manipulateRightAction);
-                    }
-                }
-            }
         }
     }
 
