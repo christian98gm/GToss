4b460505c33145660c1888c1e6ca25c8990140db
christian98gm <christian98gm@gmail.com>
2021-12-22 15:45:00 +0100

    Movement prefab ready

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Test : MonoBehaviour
+{
+
+    public ActionBasedController cameraController;
+
+
+    private XROrigin xrOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xrOrigin = GetComponent<XROrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
