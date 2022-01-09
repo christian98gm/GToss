655842b9d863560ee500f613341973244f26797c
TheLlopis16 <43938977+TheLlopis16@users.noreply.github.com>
2022-01-09 19:15:37 +0100

    Victory Condition & Menu Selection

---
+++ Assets/Canasta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.CoreModule;
+
+public class Canasta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        void OnTriggerEnter(Collider collision)
+        {
+            if(collision.tag == "Player") //Puede ser "pelota" o lo que sea que pongais como tag
+            {
+                GameObject.FindByTag("MenuAlGanar").active = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MenuAlGanar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.CoreModule;
+
+public class MenuAlGanar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindByTag(MenuAlGanar).active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
