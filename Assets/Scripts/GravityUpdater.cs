6302486cf13aa0375cf579e9f5ceb08f7d3ecad4
unknown <christian98gm@gmail.com>
2022-01-10 15:48:33 +0100

    Jupiter rescale and skybox updated. Game mechanics missing

--- Assets/Scripts/GravityUpdater.cs
+++ Assets/Scripts/GravityUpdater.cs
@@ -7,10 +7,18 @@ public class GravityUpdater : MonoBehaviour
 {
 
     public TextMeshProUGUI textUI;
+    public float defaultG = Physics.gravity.y;
+
+    private void Start()
+    {
+        Physics.gravity = new Vector3(0.0f, defaultG, 0.0f);
+    }
 
     public void GravityUpdate(float value)
     {
-        textUI.text = value.ToString("F2");
+        if(textUI != null) {
+            textUI.text = value.ToString("F2");
+        }
         Physics.gravity = new Vector3(0.0f, value, 0.0f);
     }
 }
